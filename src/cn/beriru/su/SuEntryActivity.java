ed8eb072babc4189d3ef6b8b4b7b34df6c3c00a3
lolicon <beriru@qq.com>
2014-11-27 16:07:30 +0800

    routinue code change

--- src/cn/beriru/su/SuEntryActivity.java
+++ src/cn/beriru/su/SuEntryActivity.java
@@ -9,6 +9,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import cn.beriru.annotation.Click;
 import cn.beriru.annotation.InjectView;
 import cn.beriru.annotation.Inspector;
 import cn.beriru.app.App;
@@ -17,8 +18,6 @@ import cn.beriru.su.Shell.OnShellExecuted;
 
 public class SuEntryActivity extends Activity {
 
-	
-	
 	@InjectView(R.id.resp) public TextView mResult;
 	@InjectView(R.id.input) EditText mInput;
 	@InjectView(R.id.ok) Button mOk;
@@ -27,43 +26,34 @@ public class SuEntryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shell_code);
-		mResult = (TextView) findViewById(R.id.resp);
-		mInput = (EditText) findViewById(R.id.input);
-		mOk = (Button) findViewById(R.id.ok);
-		mOk.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(final View v) {
-				new Thread(){
-					@Override
-					public void run() {
-						String cmd = mInput.getText().toString();
-						Shell.run(cmd.trim(), new OnShellExecuted(){
-							@Override
-							public void onShellExec(List<String> rets) {
-								mInput.setText("");
-								if(rets != null){
-									for(String s : rets){
-										mResult.append(s + "\n");
-									}
-								}
-							}
-
-							@Override
-							public void handleException(Throwable e) {
-								App.toast(e.getMessage());
-							}
-						});
-					}
-				}.start();
-			}
-		});
-		
 		Inspector.inspect(this);
 	}
 	
 	
-	
-	
-	
+	@Click(R.id.ok)
+	public void onClick(final View v){
+		new Thread(){
+			@Override
+			public void run() {
+				String cmd = mInput.getText().toString();
+				Shell.run(cmd.trim(), new OnShellExecuted(){
+					@Override
+					public void onShellExec(List<String> rets) {
+						mInput.setText("");
+						if(rets != null){
+							for(String s : rets){
+								mResult.append(s + "\n");
+							}
+						}
+					}
 
+					@Override
+					public void handleException(Throwable e) {
+						App.toast(e.getMessage());
+					}
+				});
+			}
+		}.start();
+	}
+		
 }
